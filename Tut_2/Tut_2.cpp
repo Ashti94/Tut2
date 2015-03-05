043d0ccfb3bd00d9f447bebfabd47287e9b68364
ashti94 <ashti_latchman@rocketmail.com>
2015-03-05 14:48:38 +0200

    commit Tut2

---
+++ Tut_2/Tut_2.cpp
@@ -0,0 +1,14 @@
+// Tut_2.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <string>
+#include "Fraction.h"
+
+using namespace std
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
